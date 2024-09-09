816c4a97283589f2a94332371be8524dda0085de
Maricris-M <maricris.mariano@awsys-i.com>
2024-09-09 12:53:01 +0800

    added exercise 1 files

---
+++ Mariano_Exercise1.java
@@ -0,0 +1,30 @@
+package JavaActivities;
+
+// import declaration
+import java.util.*;
+
+public class Mariano_Exercise1 {
+
+	public static void main(String[] args) {
+		// scanner to read input values from the user
+		Scanner input = new Scanner(System.in);
+		// variable declarations
+		int x, y, z, result;
+		
+		// get three inputs from the user
+		System.out.print("Enter the first integer: ");
+		x = input.nextInt();
+		System.out.print("Enter the second integer: ");
+		y = input.nextInt();
+		System.out.print("Enter the third integer: ");
+		z = input.nextInt();
+		input.close();
+		
+		// compute the product
+		result = x * y * z;
+		
+		// display the result
+		System.out.println("\nProduct is "+ result +".");
+		
+	} // end of main method
+} // end of class
