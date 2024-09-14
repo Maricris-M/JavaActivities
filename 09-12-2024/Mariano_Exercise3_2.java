ae5bef2b06fd642932d26a1bd230068e71d25e0a
Maricris-M <maricris.mariano@awsys-i.com>
2024-09-14 23:07:39 +0800

    added economy reservation

--- 09-12-2024/Mariano_Exercise3_2.java
+++ 09-12-2024/Mariano_Exercise3_2.java
@@ -4,27 +4,41 @@ import java.util.*;
 
 public class Mariano_Exercise3_2 {
 	
+	public static Scanner keyboard = new Scanner(System.in);
 	public static char[] seats = new char[10];
 	public static int firstClassCounter = 0;
+	public static int economyCounter = 0;
+	public static boolean seatsNotFull = true;
 	
 	public static void main(String[] args) {
 		Arrays.fill(seats, '0');
 		int type;
 		
-		while (seats[4] != 'x' && seats[9] != 'x') {
+		// firstClassCounter != 5 || economyCounter != 5 // seats[4] != 'X' || seats[9] != 'X'
+		while (seatsNotFull) {
 			type = inputType();
-			if (type == 1) {
-				firstClass();
-				System.out.println(Arrays.toString(seats));
+			if (type == 1) {				
+				if (firstClassCounter == 5 && economyCounter != 5) {
+					changeSection(type);
+				} else {
+					firstClass();
+				}
 			} else {
-				
+				if (economyCounter == 5 && firstClassCounter != 5) {
+					changeSection(type);
+				} else {
+					economy();
+				}
 			}
+			seatChecker();
 		}
 		
+		keyboard.close();
+		System.out.println("Departing in 3... 2... 1");
+		System.exit(0);
 	}
 	
 	public static int inputType() {
-		Scanner keyboard = new Scanner(System.in);
 		int classType = 0;
 	
 		do {
@@ -39,9 +53,54 @@ public class Mariano_Exercise3_2 {
 	}
 	
 	public static void firstClass() {
-		seats[firstClassCounter] = 'x';
-		firstClassCounter++;	
+		seats[firstClassCounter] = 'X';
+		firstClassCounter++;
+		System.out.println(Arrays.toString(seats));
+	}
+	
+	public static void economy() {
+		seats[economyCounter+5] = 'X';
+		economyCounter++;
+		System.out.println(Arrays.toString(seats));
+	}
+	
+	public static void changeSection(int type) {
+		String changeSection;
+		boolean inputChecker = false;
 		
+		do {
+			if (type == 1) {
+				System.out.println("First Class section is full. "
+						+ "Do you want to be placed in the Economy section? [Yes/No]: ");
+				changeSection = keyboard.next();
+				
+				if (changeSection.equalsIgnoreCase("yes")) {
+					inputChecker = true;
+					economy();
+				} else if (changeSection.equalsIgnoreCase("no")) {
+					inputChecker = true;
+					System.out.println("Next flight leaves in 3 hours.");
+				}
+			} else {
+				System.out.println("Economy section is full. "
+						+ "Do you want to be placed in the First Class section? [Yes/No]: ");
+				changeSection = keyboard.next();
+				
+				if (changeSection.equalsIgnoreCase("yes")) {
+					inputChecker = true;
+					firstClass();
+				} else if (changeSection.equalsIgnoreCase("no")) {
+					inputChecker = true;
+					System.out.println("Next flight leaves in 3 hours.");
+				}
+			}
+		} while (inputChecker == false);
+	}
+	
+	public static void seatChecker() {
+		if (firstClassCounter == 5 && economyCounter == 5) {
+			seatsNotFull = false;
+		}
 	}
 
 }
